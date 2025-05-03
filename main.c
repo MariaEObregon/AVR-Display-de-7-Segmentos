d1694535cf5b5bc4a13d35eabc76cca3ee3f76bd
Maria Obregon <mariaeduardaobregon@hotmail.com>
2025-05-03 14:16:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
-//Programação Display 7 Segmentos
+// Programação Display de 7 Segmentos
 
 #define F_CPU   16000000
 #include <util/delay.h>
 #include <avr/io.h>
 
-char digitos7seg[16] = {    0x3F, 0x06, 0x5B, 0x4F, 
-                            0x66, 0x6D, 0x7D, 0x07, 
-                            0x7F, 0x6F, 0x77, 0x7C, 
-                            0x39, 0x5E, 0x79, 0x71  };
+//Digitos do display em hexa decimal de 0 até F
+
+char dig7seg[16] = {  0x3F, 0x06, 0x5B, 0x4F,  
+                      0x66, 0x6D, 0x7D, 0x07, 
+                      0x7F, 0x6F, 0x77, 0x7C, 
+                      0x39, 0x5E, 0x79, 0x71  };
 
 void disp7seg_init( void ){
-    DDRD  = 0xFF;
-    PORTD = 0x00;
+    DDRD  = 0xFF; //Port D como saída
+    PORTD = 0x00; //Zerando o port
 }
 
 void disp7seg( unsigned char d ){
-    PORTD = digitos7seg[d];
+    PORTD = dig7seg[d];
 }
 
 int main(void) {
